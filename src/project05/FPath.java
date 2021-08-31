b20dcf210c1ae177edf7f184ede675709b32ffc2
parksunyoung <iss002@naver.com>
2021-08-31 20:44:31 +0900

    Jewelry class add.

--- src/project05/FPath.java
+++ src/project05/FPath.java
@@ -5,5 +5,5 @@ public interface FPath {
 	public String Path2 = "";
 	public String Path3 = "";
 	public String Path4 = "";
-	public String Path5 = "";
+	public String Path5 = "D:/오후 취업반 박선영/java/test/StDTO.txt";
 }
