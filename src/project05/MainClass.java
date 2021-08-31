c0085098a0421d1aeb697126e26234f09a12b57e
Fiction2st <babu6322@naver.com>
2021-08-31 20:55:42 +0900

    김형석 class 작성 완료

--- src/project05/MainClass.java
+++ src/project05/MainClass.java
@@ -2,11 +2,13 @@ package project05;
 
 import java.util.Scanner;
 
+import khs.StKHS;
+
 public class MainClass {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int input = 0;
-		
+		StKHS hs = new StKHS();
 		
 		while(true) {
 			System.out.println("1.박상진");
@@ -21,7 +23,9 @@ public class MainClass {
 			switch(input) {
 			case 1: break;
 			case 2: break;
-			case 3: break;
+			case 3: 
+				hs.display();
+				break;
 			case 4: break;
 			case 5: break;
 			case 6: System.exit(0); break;
